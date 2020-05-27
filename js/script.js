a08682d9a74f4a651db9210bbd8507426919b3f2
FRedRick97 <chopra.aakash7@gmail.com>
2020-05-28 04:02:41 +0530

    Final navbar added with responsiveness

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 $(function () {
     var button = $('.responsiveButton');
     button.on('click', function () {
-        console.log('I was clicked');
-        $('nav').toggleClass('navDisplay');
-        $('.header-logo').toggleClass('adjustHeight');
-        $('header').toggleClass('headerHeight');
+        $('#overlay').css({ "display": "block" });
+        $('#overlay').animate({ top: '0' }, 500, function () {
+            $('#overlay').css({ "display": "block" });
+        });
     });
 
+    var exit = $('.overlay-exit').on('click', function () {
+        // $('#overlay').css({ "animation": "block", "top": "0" });
 
+        // $('#overlay').addClass('demoClass');
+        // $('#overlay').css({ "animation": "demo .5s ease-in-out" });
+        // $('#overlay').css({ "display": "none" });
+
+        $('#overlay').animate({ top: '-100%' }, 500, function () {
+            $('#overlay').css({ "display": "none" });
+        });
+
+    });
 });
