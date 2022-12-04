c5ef7c32cdab83a4e9410b495e8e7de6a313f183
FRedRick97 <chopra.aakash7@gmail.com>
2022-12-04 16:04:22 +0530

    work under progress

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+    var button = $('.responsiveButton');
+    button.on('click', function () {
+        $('#overlay').css({ "display": "block" });
+        $('#overlay').animate({ top: '0' }, 500, function () {
+            $('#overlay').css({ "display": "block" });
+        });
+    });
+
+    var exit = $('.overlay-exit').on('click', function () {
+        // $('#overlay').css({ "animation": "block", "top": "0" });
+
+        // $('#overlay').addClass('demoClass');
+        // $('#overlay').css({ "animation": "demo .5s ease-in-out" });
+        // $('#overlay').css({ "display": "none" });
+
+        $('#overlay').animate({ top: '-100vh' }, 500, function () {
+            $('#overlay').css({ "display": "none" });
+        });
+
+    });
+});
